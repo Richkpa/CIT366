2cf37b7cb31bec7610450e3eafe5ebc21a1e8791
prich <richluekpakor@yahoo.com>
2017-05-08 13:22:10 -0600

    beginning of lesson3 05/8/17

--- cms/src/app/header.component.ts
+++ cms/src/app/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+//This is a new commne
   constructor() { }
 
   ngOnInit() {
